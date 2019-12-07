a6855792037ff6613eceb25d2e20ef32282232e3
jonathan-annett <jonathan.max.annett@gmail.com>
2019-12-07 18:51:05 +1100

    pasted from glitch to update repo

--- tab-calls.js
+++ tab-calls.js
@@ -1402,7 +1402,7 @@ function tabCalls () {
                           
                           function pairing_html () {/*
                           
-<span class="pairing_header">{$pair_setup_title$}<button class="pairing_button_off">X</button></span>
+<span class="pairing_header">{$pair_setup_title$}<button class="pairing_button_off">{$pair_close_btn$}</button></span>
  <input class="pairing_secret" id="secret" value="unset" name="secret" type="hidden">
  <span class="pairing_button_new_wrap">
      <button class="pairing_button_new"><i class='fas fa-redo' style='font-size:18px;color:blue'></i></button>
@@ -1986,7 +1986,9 @@ function tabCalls () {
                             "pair_sms_bottom_help",
                             "pair_email_bottom_help",
                             "pair_scan_bottom_help",
-                            "pair_qr_bottom_help" ].forEach(function(tag) {
+                            "pair_qr_bottom_help",
+                           ,"pair_close_btn"
+                           ].forEach(function(tag) {
                             var rep = self.defaults[tag];
                             if (rep) {
                                pr_html = pr_html.split('{$'+tag+'$}').join(rep);
@@ -3985,157 +3987,157 @@ function tabCalls () {
  */
 
 (function (context) {
-	if (context.Proxy) return; // return if Proxy already exist
+    if (context.Proxy) return; // return if Proxy already exist
 
-	var noop = function () {},
-		assign = Object.assign || noop,
-		getProto = Object.getPrototypeOf || noop,
-		setProto = Object.setPrototypeOf || noop;
+    var noop = function () {},
+        assign = Object.assign || noop,
+        getProto = Object.getPrototypeOf || noop,
+        setProto = Object.setPrototypeOf || noop;
 
-	/**
-	 * Throw a type error
-	 * @param {String} message
-	 */
-	function throwTypeError(message) {
-		throw new TypeError(message);
-	}
+    /**
+     * Throw a type error
+     * @param {String} message
+     */
+    function throwTypeError(message) {
+        throw new TypeError(message);
+    }
 
-	/**
-	 * The internal member constructor
-	 * @constructor
-	 * @param {Function} target
-	 * @param {Object} handler
-	 */
-	function InternalMember(target, handler) {
-		this.target = target; // [[ProxyTarget]]
-		this.handler = handler; // [[ProxyHandler]]
-	}
+    /**
+     * The internal member constructor
+     * @constructor
+     * @param {Function} target
+     * @param {Object} handler
+     */
+    function InternalMember(target, handler) {
+        this.target = target; // [[ProxyTarget]]
+        this.handler = handler; // [[ProxyHandler]]
+    }
 
-	/**
-	 * The [[Call]] internal method
-	 * @param {Object} thisArg
-	 * @param {Object} argsList
-	 */
-	InternalMember.prototype.$call = function (thisArg, argsList) {
-		var target = this.target,
-			handler = this.handler;
-		if (!handler) {
-			throwTypeError('Cannot perform \'call\' on a proxy that has been revoked');
-		}
-		if (handler.apply == null) {
-			return target.apply(thisArg, argsList);
-		} else if (typeof handler.apply === 'function') {
-			return handler.apply(target, thisArg, argsList);
-		} else {
-			throwTypeError('Proxy handler\'s apply trap must be a function');
-		}
-	};
+    /**
+     * The [[Call]] internal method
+     * @param {Object} thisArg
+     * @param {Object} argsList
+     */
+    InternalMember.prototype.$call = function (thisArg, argsList) {
+        var target = this.target,
+            handler = this.handler;
+        if (!handler) {
+            throwTypeError('Cannot perform \'call\' on a proxy that has been revoked');
+        }
+        if (handler.apply == null) {
+            return target.apply(thisArg, argsList);
+        } else if (typeof handler.apply === 'function') {
+            return handler.apply(target, thisArg, argsList);
+        } else {
+            throwTypeError('Proxy handler\'s apply trap must be a function');
+        }
+    };
 
-	/**
-	 * The [[Construct]] internal method
-	 * @param {Object} thisArg
-	 * @param {Object} argsList
-	 * @returns {Object}
-	 */
-	InternalMember.prototype.$construct = function (thisArg, argsList) {
-		var target = this.target,
-			handler = this.handler,
-			result;
-		if (!handler) {
-			throwTypeError('Cannot perform \'construct\' on a proxy that has been revoked');
-		}
-		if (handler.construct == null) {
-			result = target.apply(thisArg, argsList);
-			return result instanceof Object ? result : thisArg;
-		} else if (typeof handler.construct === 'function') {
-			result = handler.construct(target, argsList);
-			if (result instanceof Object) {
-				return result;
-			} else {
-				throwTypeError('Proxy handler\'s construct trap must return an object');
-			}
-		} else {
-			throwTypeError('Proxy handler\'s construct trap must be a function');
-		}
-	};
+    /**
+     * The [[Construct]] internal method
+     * @param {Object} thisArg
+     * @param {Object} argsList
+     * @returns {Object}
+     */
+    InternalMember.prototype.$construct = function (thisArg, argsList) {
+        var target = this.target,
+            handler = this.handler,
+            result;
+        if (!handler) {
+            throwTypeError('Cannot perform \'construct\' on a proxy that has been revoked');
+        }
+        if (handler.construct == null) {
+            result = target.apply(thisArg, argsList);
+            return result instanceof Object ? result : thisArg;
+        } else if (typeof handler.construct === 'function') {
+            result = handler.construct(target, argsList);
+            if (result instanceof Object) {
+                return result;
+            } else {
+                throwTypeError('Proxy handler\'s construct trap must return an object');
+            }
+        } else {
+            throwTypeError('Proxy handler\'s construct trap must be a function');
+        }
+    };
 
-	/**
-	 * Create a Proxy object
-	 * @param {Function} target
-	 * @param {Object} handler
-	 * @param {Object} revokeResult
-	 * @returns {Function}
-	 */
-	function createProxy(target, handler, revokeResult) {
-		// Check the type of arguments
-		if (typeof target !== 'function') {
-			throwTypeError('Proxy polyfill only support function target');
-		} else if (!(handler instanceof Object)) {
-			throwTypeError('Cannot create proxy with a non-object handler');
-		}
+    /**
+     * Create a Proxy object
+     * @param {Function} target
+     * @param {Object} handler
+     * @param {Object} revokeResult
+     * @returns {Function}
+     */
+    function createProxy(target, handler, revokeResult) {
+        // Check the type of arguments
+        if (typeof target !== 'function') {
+            throwTypeError('Proxy polyfill only support function target');
+        } else if (!(handler instanceof Object)) {
+            throwTypeError('Cannot create proxy with a non-object handler');
+        }
 
-		// Create an internal member object
-		var member = new InternalMember(target, handler);
+        // Create an internal member object
+        var member = new InternalMember(target, handler);
 
-		// Create a proxy object - `P`
-		function P() {
-			return this instanceof P ?
-				member.$construct(this, arguments) :
-				member.$call(this, arguments);
-		}
+        // Create a proxy object - `P`
+        function P() {
+            return this instanceof P ?
+                member.$construct(this, arguments) :
+                member.$call(this, arguments);
+        }
 
-		assign(P, target); // copy target's properties
-		P.prototype = target.prototype; // copy target's prototype
-		setProto(P, getProto(target)); // copy target's [[Prototype]]
+        assign(P, target); // copy target's properties
+        P.prototype = target.prototype; // copy target's prototype
+        setProto(P, getProto(target)); // copy target's [[Prototype]]
 
-		if (revokeResult) {
-			// Set the revocation function
-			revokeResult.revoke = function () {
-				member.target = null;
-				member.handler = null;
-				for (var key in P) { // delete proxy's properties
-					P.hasOwnProperty(key) && delete P[key];
-				}
-				P.prototype = {}; // reset proxy's prototype
-				setProto(P, {}); // reset proxy's [[Prototype]]
-			};
-		}
+        if (revokeResult) {
+            // Set the revocation function
+            revokeResult.revoke = function () {
+                member.target = null;
+                member.handler = null;
+                for (var key in P) { // delete proxy's properties
+                    P.hasOwnProperty(key) && delete P[key];
+                }
+                P.prototype = {}; // reset proxy's prototype
+                setProto(P, {}); // reset proxy's [[Prototype]]
+            };
+        }
 
-		return P;
-	}
+        return P;
+    }
 
-	/**
-	 * The Proxy constructor
-	 * @constructor
-	 * @param {Function} target
-	 * @param {Object} handler
-	 * @returns {Function}
-	 */
-	function Proxy(target, handler) {
-		if (this instanceof Proxy) {
-			return createProxy(target, handler);
-		} else {
-			throwTypeError('Constructor Proxy requires \'new\'');
-		}
-	}
+    /**
+     * The Proxy constructor
+     * @constructor
+     * @param {Function} target
+     * @param {Object} handler
+     * @returns {Function}
+     */
+    function Proxy(target, handler) {
+        if (this instanceof Proxy) {
+            return createProxy(target, handler);
+        } else {
+            throwTypeError('Constructor Proxy requires \'new\'');
+        }
+    }
 
-	/**
-	 * Create a revocable Proxy object
-	 * @param {Function} target
-	 * @param {Object} handler
-	 * @returns {{proxy, revoke}}
-	 */
-	Proxy.revocable = function (target, handler) {
-		var result = {};
-		result.proxy = createProxy(target, handler, result);
-		return result;
-	};
+    /**
+     * Create a revocable Proxy object
+     * @param {Function} target
+     * @param {Object} handler
+     * @returns {{proxy, revoke}}
+     */
+    Proxy.revocable = function (target, handler) {
+        var result = {};
+        result.proxy = createProxy(target, handler, result);
+        return result;
+    };
 
-	context.Proxy = Proxy;
+    context.Proxy = Proxy;
 }(
-	typeof window === 'object' ?
-		window :
-		typeof global === 'object' ? global : this // using `this` for web workers & supports Browserify / Webpack
+    typeof window === 'object' ?
+        window :
+        typeof global === 'object' ? global : this // using `this` for web workers & supports Browserify / Webpack
 ));
 
 
