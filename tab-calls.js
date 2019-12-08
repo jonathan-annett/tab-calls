2a86e26a4091532bd0d9bfe0c4f2f8c5d9533553
jonathan-annett <jonathan.max.annett@gmail.com>
2019-12-08 19:39:26 +1100

    work in progress - config css rework

--- tab-calls.js
+++ tab-calls.js
@@ -14,6 +14,7 @@ function tabCalls () {
       var OK = Object_polyfills().OK,DP=Object_polyfills.DP,HIDE=Object_polyfills.HIDE;
       Array_polyfills();
       String_polyfills();
+      Proxy_polyfill();
             
       return browserExports("messages") || nodeJSExports("messages");
   
@@ -259,6 +260,7 @@ function tabCalls () {
           return Object.keys(localStorage).filter(isSenderId);
       }
       
+      
       function isRemoteSenderId(k){
           if (k.startsWith(remote_tab_id_prefix) && k.contains(remote_tab_id_delim) ) {
               return ["tabRemoteCallViaWS"].contains(localStorage[k]);
@@ -269,6 +271,7 @@ function tabCalls () {
       function remoteSenderIds(){
           return Object.keys(localStorage).filter(isRemoteSenderId);
       }
+      
       
       function isLocalSenderId(k){
           if (k.startsWith(tab_id_prefix)) {
@@ -944,6 +947,7 @@ function tabCalls () {
           return cmd.substr (0,ix)+scan+deviceId+"."+cmd.substr(ix+scan.length);
       }
       
+      
       function cmdSource (cmd){
           // generalized extraction of from field in formal JSON
           // this is optimized and assumes the from field is near the end of the JSON
@@ -957,6 +961,7 @@ function tabCalls () {
           if (ix < 0) return false;
           return work.substr(0,ix);
       }
+      
   
       function browserExports (defaultPrefix) {
           if  (  (typeof process==='object' ) ||
@@ -3492,170 +3497,176 @@ function tabCalls () {
               Object.defineProperties(String.prototype,polyfills);
           }
       }
+      
+      function Proxy_polyfill() {
+          /**
+           * ES6 Proxy Polyfill
+           * @version 1.2.1
+           * @author Ambit Tsai <ambit_tsai@qq.com>
+           * @license Apache-2.0
+           * @see {@link https://github.com/ambit-tsai/es6-proxy-polyfill}
+           */
+          
+          (function (context) {
+              if (context.Proxy) return; // return if Proxy already exist
+          
+              var 
+              
+              noop = function () {},
+              assign = Object.assign || noop,
+              getProto = Object.getPrototypeOf || noop,
+              setProto = Object.setPrototypeOf || noop;
+          
+              /**
+               * Throw a type error
+               * @param {String} message
+               */
+              function throwTypeError(message) {
+                  throw new TypeError(message);
+              }
+          
+              /**
+               * The internal member constructor
+               * @constructor
+               * @param {Function} target
+               * @param {Object} handler
+               */
+              function InternalMember(target, handler) {
+                  this.target = target; // [[ProxyTarget]]
+                  this.handler = handler; // [[ProxyHandler]]
+              }
+          
+              /**
+               * The [[Call]] internal method
+               * @param {Object} thisArg
+               * @param {Object} argsList
+               */
+              InternalMember.prototype.$call = function (thisArg, argsList) {
+                  var target = this.target,
+                      handler = this.handler;
+                  if (!handler) {
+                      throwTypeError('Cannot perform \'call\' on a proxy that has been revoked');
+                  }
+                  if (handler.apply === null) {
+                      return target.apply(thisArg, argsList);
+                  } else if (typeof handler.apply === 'function') {
+                      return handler.apply(target, thisArg, argsList);
+                  } else {
+                      throwTypeError('Proxy handler\'s apply trap must be a function');
+                  }
+              };
+          
+              /**
+               * The [[Construct]] internal method
+               * @param {Object} thisArg
+               * @param {Object} argsList
+               * @returns {Object}
+               */
+              InternalMember.prototype.$construct = function (thisArg, argsList) {
+                  var target = this.target,
+                      handler = this.handler,
+                      result;
+                  if (!handler) {
+                      throwTypeError('Cannot perform \'construct\' on a proxy that has been revoked');
+                  }
+                  if (handler.construct === null) {
+                      result = target.apply(thisArg, argsList);
+                      return result instanceof Object ? result : thisArg;
+                  } else if (typeof handler.construct === 'function') {
+                      result = handler.construct(target, argsList);
+                      if (result instanceof Object) {
+                          return result;
+                      } else {
+                          throwTypeError('Proxy handler\'s construct trap must return an object');
+                      }
+                  } else {
+                      throwTypeError('Proxy handler\'s construct trap must be a function');
+                  }
+              };
+          
+              /**
+               * Create a Proxy object
+               * @param {Function} target
+               * @param {Object} handler
+               * @param {Object} revokeResult
+               * @returns {Function}
+               */
+              function createProxy(target, handler, revokeResult) {
+                  // Check the type of arguments
+                  if (typeof target !== 'function') {
+                      throwTypeError('Proxy polyfill only support function target');
+                  } else if (!(handler instanceof Object)) {
+                      throwTypeError('Cannot create proxy with a non-object handler');
+                  }
+          
+                  // Create an internal member object
+                  var member = new InternalMember(target, handler);
+          
+                  // Create a proxy object - `P`
+                  function P() {
+                      return this instanceof P ?
+                          member.$construct(this, arguments) :
+                          member.$call(this, arguments);
+                  }
+          
+                  assign(P, target); // copy target's properties
+                  P.prototype = target.prototype; // copy target's prototype
+                  setProto(P, getProto(target)); // copy target's [[Prototype]]
+          
+                  if (revokeResult) {
+                      // Set the revocation function
+                      revokeResult.revoke = function () {
+                          member.target = null;
+                          member.handler = null;
+                          for (var key in P) { // delete proxy's properties
+                              P.hasOwnProperty(key) && delete P[key];
+                          }
+                          P.prototype = {}; // reset proxy's prototype
+                          setProto(P, {}); // reset proxy's [[Prototype]]
+                      };
+                  }
+          
+                  return P;
+              }
+          
+              /**
+               * The Proxy constructor
+               * @constructor
+               * @param {Function} target
+               * @param {Object} handler
+               * @returns {Function}
+               */
+              function Proxy(target, handler) {
+                  if (this instanceof Proxy) {
+                      return createProxy(target, handler);
+                  } else {
+                      throwTypeError('Constructor Proxy requires \'new\'');
+                  }
+              }
+          
+              /**
+               * Create a revocable Proxy object
+               * @param {Function} target
+               * @param {Object} handler
+               * @returns {{proxy, revoke}}
+               */
+              Proxy.revocable = function (target, handler) {
+                  var result = {};
+                  result.proxy = createProxy(target, handler, result);
+                  return result;
+              };
+          
+              context.Proxy = Proxy;
+          }(
+              typeof window === 'object' ?
+                  window :
+                  typeof global === 'object' ? global : this // using `this` for web workers & supports Browserify / Webpack
+          ));
+                    
+      }
   
   }
 
-/**
- * ES6 Proxy Polyfill
- * @version 1.2.1
- * @author Ambit Tsai <ambit_tsai@qq.com>
- * @license Apache-2.0
- * @see {@link https://github.com/ambit-tsai/es6-proxy-polyfill}
- */
-
-(function (context) {
-    if (context.Proxy) return; // return if Proxy already exist
-
-    var noop = function () {},
-        assign = Object.assign || noop,
-        getProto = Object.getPrototypeOf || noop,
-        setProto = Object.setPrototypeOf || noop;
-
-    /**
-     * Throw a type error
-     * @param {String} message
-     */
-    function throwTypeError(message) {
-        throw new TypeError(message);
-    }
-
-    /**
-     * The internal member constructor
-     * @constructor
-     * @param {Function} target
-     * @param {Object} handler
-     */
-    function InternalMember(target, handler) {
-        this.target = target; // [[ProxyTarget]]
-        this.handler = handler; // [[ProxyHandler]]
-    }
-
-    /**
-     * The [[Call]] internal method
-     * @param {Object} thisArg
-     * @param {Object} argsList
-     */
-    InternalMember.prototype.$call = function (thisArg, argsList) {
-        var target = this.target,
-            handler = this.handler;
-        if (!handler) {
-            throwTypeError('Cannot perform \'call\' on a proxy that has been revoked');
-        }
-        if (handler.apply === null) {
-            return target.apply(thisArg, argsList);
-        } else if (typeof handler.apply === 'function') {
-            return handler.apply(target, thisArg, argsList);
-        } else {
-            throwTypeError('Proxy handler\'s apply trap must be a function');
-        }
-    };
-
-    /**
-     * The [[Construct]] internal method
-     * @param {Object} thisArg
-     * @param {Object} argsList
-     * @returns {Object}
-     */
-    InternalMember.prototype.$construct = function (thisArg, argsList) {
-        var target = this.target,
-            handler = this.handler,
-            result;
-        if (!handler) {
-            throwTypeError('Cannot perform \'construct\' on a proxy that has been revoked');
-        }
-        if (handler.construct === null) {
-            result = target.apply(thisArg, argsList);
-            return result instanceof Object ? result : thisArg;
-        } else if (typeof handler.construct === 'function') {
-            result = handler.construct(target, argsList);
-            if (result instanceof Object) {
-                return result;
-            } else {
-                throwTypeError('Proxy handler\'s construct trap must return an object');
-            }
-        } else {
-            throwTypeError('Proxy handler\'s construct trap must be a function');
-        }
-    };
-
-    /**
-     * Create a Proxy object
-     * @param {Function} target
-     * @param {Object} handler
-     * @param {Object} revokeResult
-     * @returns {Function}
-     */
-    function createProxy(target, handler, revokeResult) {
-        // Check the type of arguments
-        if (typeof target !== 'function') {
-            throwTypeError('Proxy polyfill only support function target');
-        } else if (!(handler instanceof Object)) {
-            throwTypeError('Cannot create proxy with a non-object handler');
-        }
-
-        // Create an internal member object
-        var member = new InternalMember(target, handler);
-
-        // Create a proxy object - `P`
-        function P() {
-            return this instanceof P ?
-                member.$construct(this, arguments) :
-                member.$call(this, arguments);
-        }
-
-        assign(P, target); // copy target's properties
-        P.prototype = target.prototype; // copy target's prototype
-        setProto(P, getProto(target)); // copy target's [[Prototype]]
-
-        if (revokeResult) {
-            // Set the revocation function
-            revokeResult.revoke = function () {
-                member.target = null;
-                member.handler = null;
-                for (var key in P) { // delete proxy's properties
-                    P.hasOwnProperty(key) && delete P[key];
-                }
-                P.prototype = {}; // reset proxy's prototype
-                setProto(P, {}); // reset proxy's [[Prototype]]
-            };
-        }
-
-        return P;
-    }
-
-    /**
-     * The Proxy constructor
-     * @constructor
-     * @param {Function} target
-     * @param {Object} handler
-     * @returns {Function}
-     */
-    function Proxy(target, handler) {
-        if (this instanceof Proxy) {
-            return createProxy(target, handler);
-        } else {
-            throwTypeError('Constructor Proxy requires \'new\'');
-        }
-    }
-
-    /**
-     * Create a revocable Proxy object
-     * @param {Function} target
-     * @param {Object} handler
-     * @returns {{proxy, revoke}}
-     */
-    Proxy.revocable = function (target, handler) {
-        var result = {};
-        result.proxy = createProxy(target, handler, result);
-        return result;
-    };
-
-    context.Proxy = Proxy;
-}(
-    typeof window === 'object' ?
-        window :
-        typeof global === 'object' ? global : this // using `this` for web workers & supports Browserify / Webpack
-));
 
 
 tabCalls();
