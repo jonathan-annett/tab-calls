d3910baf2c835f2a0524a94ab7f99de62dabf163
jonathan-annett <jonathan.max.annett@gmail.com>
2019-12-17 11:13:18 +1100

    changed sleeping and focused events name to generic update event

--- tab-calls.js
+++ tab-calls.js
@@ -3027,13 +3027,19 @@ function tabCalls (currentlyDeployedVersion) {
                           };
                       
                           makeCode();
-                          
+                          /*
                           self.variables.addEventListener("sleeping",function(id,key,value){
                               console_log((id?id:"this tab")+" is "+(value?"sleeping":"awake"));    
                           });
                           
                           self.variables.addEventListener("focused",function(id,key,value){
                               console_log((id?id:"this tab")+" is "+(value?"focused":"blurred"));    
+                          });
+                          
+                          */
+                          
+                          self.variables.addEventListener("update",function(e){
+                              console_log(JSON.stringify(e));
                           });
                           
                           
