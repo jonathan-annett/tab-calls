f2f076d5f459dc3d9b79e275d8981974d5d177b9
jonathan-annett <jonathan.max.annett@gmail.com>
2019-12-25 23:54:16 +1100

    ...

--- src/@browserExports.js/classProxy.js
+++ src/@browserExports.js/classProxy.js
@@ -229,7 +229,6 @@ function getWatchElementClassName(api) {
     api.__watchElementClassName = watchElementClassName;
     api.__setElementClassName   = setElementClassName;
     api.__elementClassListOp    = elementClassListOp;
-    watchElementClassName._persistent = true;
     
     return watchElementClassName;
     
@@ -264,6 +263,7 @@ function getWatchElementClassName(api) {
             }
 
             cbs[callInfo.from] = callback;
+            callback._persistent = true;
             console.log(query,"<<<",element.className);
             callback(undefined, element.className);
         } else {
--- tab-calls.js
+++ tab-calls.js
@@ -1508,7 +1508,6 @@ function getWatchElementClassName(api) {
     api.__watchElementClassName = watchElementClassName;
     api.__setElementClassName   = setElementClassName;
     api.__elementClassListOp    = elementClassListOp;
-    watchElementClassName._persistent = true;
     
     return watchElementClassName;
     
@@ -1543,6 +1542,7 @@ function getWatchElementClassName(api) {
             }
 
             cbs[callInfo.from] = callback;
+            callback._persistent = true;
             console.log(query,"<<<",element.className);
             callback(undefined, element.className);
         } else {
